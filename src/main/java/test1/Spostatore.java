619975579edef168e1b61f416aac855857815217
Alessandro <asbacco@gmail.com>
2015-11-11 15:24:55 +0100

    Ho aggiunto i file java

---
+++ src/main/java/test1/Spostatore.java
@@ -0,0 +1,17 @@
+package test1;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public interface Spostatore {   //un interfaccia rappresenta un concetto astratta, non ha attributi
+	                           // e non vengono definiti i corpi dei METODI nella classe INTERFACCIA
+	                           // posso soloo DICHIARARE METODI
+
+	public void muovi();
+	//significa che se una classe EREDITA da Automobile DEVE IMPLEMENTARE e DEFINIRE per forza un 
+	// metodo MUOVI()
+
+	//public String getTipo();
+	
+	
+	
+}
